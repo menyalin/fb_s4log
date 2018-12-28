71cf2a5922537beab93504dd453318b3a1fc8163
menyalin <menyalin@gmail.com>
2018-12-28 22:21:28 +0300

    #

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,14 @@
 <template>
   <v-toolbar dark app dense>
     <v-toolbar-side-icon/>
-    <v-toolbar-title><router-link to="/" >s4log</router-link></v-toolbar-title>
+    <v-toolbar-title>
+      <router-link to="/">s4log</router-link>
+    </v-toolbar-title>
     <v-spacer/>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat to="/admin">Админка</v-btn>
       <v-btn v-if="!loggedIn" flat to="/auth/login">Login</v-btn>
-      <v-btn v-if="loggedIn" disabled flat @click="logout">Logout</v-btn>
+      <v-btn v-if="loggedIn" flat @click="logout">Logout</v-btn>
 
       <v-btn flat to="/test">test</v-btn>
     </v-toolbar-items>
@@ -22,7 +24,13 @@
     },
     methods: {
       logout () {
-        this.$store.dispatch('logout')
+        this.$confirm('Вы уверены?')
+        .then((res) => {
+          console.log(res)
+          if (res) {
+            this.$store.dispatch('logout')
+          }
+        })
       }
     }
   }
